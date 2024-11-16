4c99fc5c21beb5e729d40d7453b1133a8ed32598
Jesusnoelrh <jesusecomerce12@gmail.com>
2024-11-16 12:24:55 -0400

    index.js

--- index.js
+++ index.js
@@ -42,12 +42,12 @@ if (randomNumber1> randomNumber2){
 
     document.querySelector("h1").innerHTML="Player 1 Wins!"
 }
-if (randomNumber1 < randomNumber2){
+else if (randomNumber1 < randomNumber2){
 
     document.querySelector("h1").innerHTML="Player 2 Wins"
 }
 
-else {
+else if (randomNumber1 === randomNumber2){
 
     document.querySelector("h1").innerHTML="Draw!"    
 
