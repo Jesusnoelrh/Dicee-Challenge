e6f49d4902125e2daa8a317ced919bd66ee4b89b
Jesusnoelrh <jesusecomerce12@gmail.com>
2024-11-16 11:46:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -19,26 +19,26 @@ if (randomNumber1 === 6){
 }
 
 var randomNumber2= Math.round(Math.random()*6);
-if (randomNumber1 === 1 || randomNumber1 === 0){
+if (randomNumber2 === 1 || randomNumber2 === 0){
     document.querySelector(".img2").setAttribute("src", "./images/dice1.png");
 }
-if (randomNumber1 === 2){
+if (randomNumber2 === 2){
     document.querySelector(".img2").setAttribute("src", "./images/dice2.png");
 }
-if (randomNumber1 === 3){
+if (randomNumber2 === 3){
     document.querySelector(".img2").setAttribute("src", "./images/dice3.png");
 }
-if (randomNumber1 === 4){
+if (randomNumber2 === 4){
     document.querySelector(".img2").setAttribute("src", "./images/dice4.png");
 }
-if (randomNumber1 === 5){
+if (randomNumber2 === 5){
     document.querySelector(".img2").setAttribute("src", "./images/dice5.png");
 }
-if (randomNumber1 === 6){
+if (randomNumber2 === 6){
     document.querySelector(".img2").setAttribute("src", "./images/dice6.png");
 }
 
-if (randomNumber1 > randomNumber2){
+if (randomNumber1> randomNumber2){
 
     document.querySelector("h1").innerHTML="Player 1 Wins!"
 }
